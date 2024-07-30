baf442ecad59b90e4aa733991bf4aecfccec969c
HugoNCastro <hugonaaman@gmail.com>
2024-07-30 20:45:35 -0300

    Lida com reset do formulário
    Altera componente Alert vindo do shadcn
    
    Adiciona mensagens de erro ao formulário
    
    Envia dados de resposta pela action

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -1,48 +1,5 @@
-import { Label } from '@radix-ui/react-label'
-import { Separator } from '@radix-ui/react-separator'
-import Image from 'next/image'
-import Link from 'next/link'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { signInWithEmailandPassword } from './actions'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const githubIcon = require('@/assets/github-icon.svg') as string
+import { SignInForm } from './sign-in-form'
 
 export default function SignInPage() {
-return (
-    <form action={signInWithEmailandPassword} className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
-      </div>
-
-      <Button className="w-full" type="submit">
-        Sign in with e-mail
-      </Button>
-
-      <Button className="w-full" variant="link" asChild size="sm">
-        <Link href="/auth/sign-up">Create new account</Link>
-      </Button>
-
-      <Separator />
-
-      <Button className="w-full" variant="outline" type="submit">
-        <Image alt="" src={githubIcon} className="mr-2 size-4 dark:invert" />
-        Sign in with GitHub
-      </Button>
-    </form>
-  )
+  return <SignInForm />
 }
