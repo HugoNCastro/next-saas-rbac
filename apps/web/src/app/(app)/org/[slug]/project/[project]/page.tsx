a5d9743aa9a5ad18c83d1d5126b9384762e3bc4b
HugoNCastro <hugonaaman@gmail.com>
2024-08-12 17:14:48 -0300

    Cria rota de interceptação para criar orgnização
    Cria component sheet
    
    Altera componente exibição do layoyt e passa header para páginas
    
    Cria componente project switcher

--- apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
@@ -1,10 +1,10 @@
 import { Header } from '@/components/header'
 
-export default async function Projetct() {
+export default function Project() {
   return (
     <div className="space-y-4 py-4">
       <Header />
-      <main className="mx-auto w-full max-w-[1280px]">Projects</main>
+      <main className="mx-auto w-full max-w-[1280px] space-y-4">Project</main>
     </div>
   )
 }
